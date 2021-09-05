aad7bb2ea928f8446d39bb28e44d68bd81f236c6
Mehdih77 <mehdiwork77@gmail.com>
2021-09-05 20:21:45 +0430

    final project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {useState, useEffect} from 'react';
-import './App.css';
 import DataTable from './components/DataTable';
 
 function App() {
 
   const [data, setData] = useState([]);
   const [q, setQ] = useState('');
+  const [searchColumns, setSearchColumns] = useState(['name'])
 
   useEffect(() => {
     fetch('https://swapi.dev/api/people/?format=json')
@@ -14,16 +14,39 @@ function App() {
   }, [])
 
   const search = (rows) => {
-    // get heading or keys  like name & height and ...
-    const columns = rows[0] && Object.keys(rows[0]);
     // for searching by any of keys
-    return rows.filter((row) => columns.some((column) => row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1))
+    return rows.filter((row) => 
+    searchColumns.some(
+      (column) => row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
+      )
+    )
+  }
+
+  // get heading or keys  like name & height and ...
+  const columns = data[0] && Object.keys(data[0]);
+
+  const handleCheckboxFilter = (column) => (e) => {
+    const checked = searchColumns.includes(column);
+    setSearchColumns((prev) => (
+      checked 
+      ?  prev.filter(sc => sc !== column)
+      : [...prev, column]
+    ))
   }
 
   return (
     <div>
        <div>
         <input type="text" value={q} onChange={(e) => setQ(e.target.value)}/>
+        { columns && 
+         columns.map((column) => (
+           <label>
+             <input type="checkbox"
+             checked={searchColumns.includes(column)}
+             onChange={handleCheckboxFilter(column)} />
+             {column}
+           </label>
+         ))}
        </div>
         <div>
           <DataTable data={search(data)}/>
@@ -33,3 +56,5 @@ function App() {
 }
 
 export default App;
+
+// can use it insted of another fetch url >>>>  https://my.api.mockaroo.com/users.json?key=77f7cd00
