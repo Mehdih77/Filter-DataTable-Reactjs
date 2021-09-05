b1d7866b648d89d4724c047c81b73d8186e65d90
Mehdih77 <mehdiwork77@gmail.com>
2021-09-05 19:20:14 +0430

    add some styles & get data from Api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import DataTable from './components/DataTable';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [q, setQ] = useState('');
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people/?format=json')
+    .then(response => response.json())
+    .then(d => setData(d.results))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="text" value={q} onChange={(e) => setQ(e.target.value)} />
+      </div>
+      <div>
+        <DataTable data={data} />
+      </div>
     </div>
   );
 }
